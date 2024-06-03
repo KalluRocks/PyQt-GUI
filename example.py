858178b53b1c963ca05b85d7befd593296b74890
KalluRocks <kalyani.utkarsh.gaikwad@gmail.com>
2024-06-03 21:28:54 +0530

    updated

--- example.py
+++ example.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication([])
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    widget.resize(600, 400)
     widget.show()
 
     sys.exit(app.exec())
